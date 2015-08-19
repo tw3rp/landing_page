f00d0b6c9bdef9e1f89b428b97c6bad6f8b23d7b
tw3rp <rahuldprabhu@gmail.com>
2015-08-19 14:24:54 -0700

    fixed mongo hopefully

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ var mongoose= require('mongoose');
 var models = require('../models/schema');
 
 var conn= mongoose.connection;
-var uri='mongodb://localhost/schema';
+var uri='mongodb://heroku_vfp6w0c0:6ghh5u7fmejj6r1jjt5030vpk7@ds033143.mongolab.com:33143/heroku_vfp6w0c0';
 
 conn.on('error',console.error.bind(console,"mongoose connection error"));
 conn.once('open',function(){
