65736e84347c30c0fc9bfbf0adbdf7823d6dc331
System Administrator <root@Rahuls-MacBook-Air.local>
2015-08-31 11:22:22 -0700

    added redis

--- server.py
+++ server.py
@@ -5,7 +5,12 @@ import socket
 import json
 import requests
 import sendgrid
+import os
+import redis
 #ff
+
+redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6879')
+redis = redis.from_url(redis_url)
 class MyTCPServer(SocketServer.ThreadingTCPServer):
   allow_reuse_address=True
 class MyTCPServerHandler(SocketServer.BaseRequestHandler):
@@ -46,5 +51,5 @@ def backup_email(email,subject,text,name):
   message.set_text(text)
   sg.send(message)
 if __name__ == '__main__':
-  server = MyTCPServer(('http://localhost',6879),MyTCPServerHandler)
+  server = MyTCPServer((redis_url,6879),MyTCPServerHandler)
   server.serve_forever()
