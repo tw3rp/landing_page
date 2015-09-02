e703d8468ebcb4e0b7d92174bbef50fe8e8d2021
System Administrator <root@Rahuls-MacBook-Air.local>
2015-09-01 18:35:10 -0700

    added a route for maps

--- config/routes.js
+++ config/routes.js
@@ -3,12 +3,14 @@ var index = require('../routes/index');
 var pictures = require('../routes/pictures')
 var todo=require('../routes/todo');
 var email=require('../routes/email');
+var maps=require('../routes/maps');
 //make the router modular so I can add more routes for my projects 
 var router = { setupRoutes: function(app){
 				app.use('/',index);
 				app.use('/pic_webapp',pictures);	
 				app.use('/todo',todo);			
 		    app.use('/email',email);
+        app.use('/maps',maps);
     }
 	};
 
---
+++ routes/maps.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+// home page 
+router.get('/', function(req,res,next){
+  res.sendFile('maps.html', {root:path.join(__dirname,'../public')});
+});
+  
